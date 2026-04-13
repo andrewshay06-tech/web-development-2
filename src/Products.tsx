6460a1df616de41e878cf815fdc4338b87313bc6
Andrew Thigpen <andrewshay06@gmail.com>
2026-04-12 23:49:51 -0400

    Added bootstrap

--- src/Products.tsx
+++ src/Products.tsx
@@ -1,34 +1,38 @@
+import { Container, Table } from "react-bootstrap";
+
 function Products() {
     return (
-        <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Product Name</th>
-                        <th>Cost</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Apple</td>
-                        <td>Fruit</td>
-                        <td>2.50</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Pineapple</td>
-                        <td>Fruit</td>
-                        <td>3.25</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Cucumber</td>
-                        <td>Vegetable</td>
-                        <td>.50</td>
-                    </tr>
-                </tbody>    
-        </table>
+        <Container className="py-4 px-3 mx-auto">
+            <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Product Name</th>
+                            <th>Cost</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Apple</td>
+                            <td>Fruit</td>
+                            <td>2.50</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Pineapple</td>
+                            <td>Fruit</td>
+                            <td>3.25</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Cucumber</td>
+                            <td>Vegetable</td>
+                            <td>.50</td>
+                        </tr>
+                    </tbody>    
+            </Table>
+        </Container>
     );
 }
 
